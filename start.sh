d84d8ab76ddb7800933263b86080a52c03312878
okuiwr <120346865+okuiwr@users.noreply.github.com>
2022-12-12 01:39:26 +0000

    Update start.sh

--- start.sh
+++ start.sh
@@ -4,7 +4,7 @@
 export UUID=587a9712-6b79-4049-86c8-353d0793ce9f
 
 #请修改为自己设置的伪装站，不要带https://
-export ProxySite=www.arsenalfclatestnews.com
+export ProxySite=www.gameshedge.com
 
 #端口，如无特殊需求请勿更改,如果要改请一并修改dockerfile中的端口
 export Port=8080
